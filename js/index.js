3dbafa2ec2cc246c84af48862d6cc9b74cf89cf8
Sourabh Gour <kumarsourabh.edu@gmail.com>
2017-01-25 11:50:29 +0530

    Finishing main page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+	document.getElementsByClassName("shoefootball")[0].style.animationName = "fade";
+}, 2000);
+setTimeout(function(){
+	document.getElementsByClassName("content")[0].style.backgroundImage = "url('images/background3.jpg')";
+}, 4500);
+setTimeout(function(){
+	document.getElementsByClassName("head")[0].innerHTML ="<h1>1-5<sup>th</sup> MARCH SVVV's Spardha</h1><h2 class='game'>The Game is On!</h2>";
+}, 5500);
